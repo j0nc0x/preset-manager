e8a92ec1e8c86656b0e504ffb4d7c6793a83cda3
Jonathan <joncoxhome@gmail.com>
2018-07-27 14:29:23 +1000

    Quick utility methods to retrieve the node type and category in string form

--- manager.py
+++ manager.py
@@ -4,8 +4,10 @@ import hou
 class PresetManager(object):
     def __init__(self, _node):
         self.preset_node = _node
+        self.local_presets = list()
+        self.remote_presets = list()
 
-    def isValid(self):
+    def is_valid(self):
         """
         Check if the preset manager has a valid node as an input
 
@@ -16,4 +18,34 @@ class PresetManager(object):
             return True
         else:
             return False
+
+    def get_node_type(self, string=True):
+        """
+        Find the node type for the given node
+
+        :param string:  Should the result be returned as a string, otherwise return the Houdini object
+        :type string:   bool
+        :return:        The node type
+        :type:          hou.NodeType
+        """
+        if self.is_valid():
+            if string:
+                return self.preset_node.type().name()
+            else:
+                return self.preset_node.type()
+
+    def get_node_type_category(self, string=True):
+        """
+        Find the node type category for the given node
+
+        :param string:  Should the result be returned as a string, otherwise return the Houdini object
+        :type string:   bool
+        :return:        The node type category
+        :type:          hou.NodeTypeCategory
+        """
+        if self.is_valid():
+            if string:
+                return self.get_node_type(string=False).category().name()
+            else:
+                return self.get_node_type(string=False).category()
 
